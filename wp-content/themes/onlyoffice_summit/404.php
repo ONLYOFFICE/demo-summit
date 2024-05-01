9276745231174aaa2ff968766e1482ceb48f9e8f
DenisTyan <denistyan16@gmail.com>
2024-05-01 14:44:31 +0500

    refactor(/404): refactor page

--- wp-content/themes/onlyoffice_summit/404.php
+++ wp-content/themes/onlyoffice_summit/404.php
@@ -1,17 +1,14 @@
 <?php get_header(); ?>
-<main class="main">
-    <img class="vectors" src="<?php echo ONLYOFFICE_IMG_DIR ?>/top-vectors.svg" alt="three vectors" />
-    <section class="invalid">
-        <div class="container">
-        <img src="<?php echo ONLYOFFICE_IMG_DIR ?>/404.svg" alt="agenda date">
-            <h1 class="invalid__title">404 error</h1>
-            <p class="invalid__description">
-            It seems you clicked on an invalid link, or entered an address that is not on this website.
-            </p>
-            <a href="/" class="home-page"
-          ><button>go to home page</button></a>
-        </div>
-    </section>
-   
+<main class="main main-error">
+  <section class="error">
+    <div class="container">
+      <div class="error__wrapper">
+        <img class="error__img" src="<?php echo ONLYOFFICE_IMG_DIR ?>/404.svg" alt="error-img" aria-hidden="true">
+        <h1 class="error__title">404 error</h1>
+        <p class="error__description">It seems you clicked on an invalid link, or entered an address that is not on this website.</p>
+        <a href="/" class="error__button button button--primary">Go to home page</a>
+      </div>
+    </div>
+  </section>
 </main>
 <?php get_footer(); ?>
